fb0013e264c93d699cf6221d6e3b22f181d35636
Blair-White <blairjwhite@hotmail.com>
2021-01-23 14:55:56 -0500

    Added some scripts for game controllers
    -Gameplay controller
    -intro controller
    -ui controller
    -main menu controller

---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Blair/GameplayController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public GameObject uiController, player;
+
+    public enum GameStates { EnterInit, Init, EnterGameplay, Gameplay, EnterLevelComplete, LevelComplete, EnterDeath, Death, Paused}
+    public GameStates State;
+
+    private void Start()
+    {
+        State = GameStates.EnterInit;
+    }
+    private void Update()
+    {
+        switch (State)
+        {
+            case GameStates.EnterInit:
+                break;
+            case GameStates.Init:
+                break;
+            case GameStates.EnterGameplay:
+                break;
+            case GameStates.Gameplay:
+                break;
+            case GameStates.EnterLevelComplete:
+                break;
+            case GameStates.LevelComplete:
+                break;
+            case GameStates.EnterDeath:
+                break;
+            case GameStates.Death:
+                break;
+            case GameStates.Paused:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PlayerDied()
+    {
+
+    }
+
+    private void LevelCompleted()
+    { 
+    
+    }
+
+    private void GamePaused()
+    {
+
+    }
+
+}
---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Blair/UiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+
+    public GameObject scorePanel, scoreText, healthBar, joystick, pause;
+    public GameObject[] lives;
+    public GameObject playerFeedBackPrefab, collectItemEffectPrefab, endGamePrefab, levelCompletePrefab;
+
+
+}
