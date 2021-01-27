bcfe163df36d81464156a4a1b8c325e3ba3d1089
Lukatiel <54953809+Lukatiel@users.noreply.github.com>
2021-01-27 02:51:57 -0500

    Added Sample Map
    - Added A sample map, made with cubes, that is like an endless runner (for the time being).
    
    - Added very basic Player movement and camera following. The player for the time being is just a sphere.
    
    -Added the following scripts:
    SpawnManager, SampleCameraController, SamplePlayerController, LevelSegmentSpawner

---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Luka/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    LevelSegmentSpawner levelSegmentSpawner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelSegmentSpawner = GetComponent<LevelSegmentSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTriggerEntered()
+    {
+        levelSegmentSpawner.MoveSegment();
+    }
+}
