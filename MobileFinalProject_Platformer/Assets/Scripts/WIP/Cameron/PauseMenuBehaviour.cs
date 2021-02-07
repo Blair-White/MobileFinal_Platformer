90451ec55d1a0aa8a03f733fc30047aa185bae98
Acercam20 <cameronakey@hotmail.ca>
2021-02-07 01:24:17 -0500

    Pause UI Implementation
    Added a button on screen to pause the game and bring up the pause menu, as well as functional buttons within the pause menu.

---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Cameron/PauseMenuBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    private GameObject UiController;
+    void Start()
+    {
+        UiController = GameObject.FindWithTag("UiController");
+    }
+
+    public void OnPressedInGamePauseButton()
+    {
+        UiController.GetComponent<UiControllerBehaviour>().PauseGame();
+    }
+
+    public void OnPressedInGameResumeButton()
+    {
+        UiController.GetComponent<UiControllerBehaviour>().ResumeGame();
+    }
+
+    public void OnPressedMainMenuButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Cameron/UiControllerBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiControllerBehaviour : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    public GameObject hudCanvas;
+    void Start()
+    {
+        pauseCanvas.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        hudCanvas.SetActive(false);
+        pauseCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        hudCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        
+    }
+}
