cf2c8681b59e9d28ee4eb80152d991be4605e03d
Catt Symonds <catherine.symonds.thompson@gmail.com>
2021-02-21 21:12:57 -0500

    audio assets and death plane

---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Cat/DeathPlane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    [SerializeField]
+    Transform portalTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = portalTo.position;
+            other.gameObject.transform.rotation = portalTo.rotation;
+        }
+    }
+}
