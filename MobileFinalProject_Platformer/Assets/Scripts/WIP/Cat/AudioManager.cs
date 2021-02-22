28a0eeddbd25e97c6bccf5fe1a09afd2dbb3f299
Catt Symonds <catherine.symonds.thompson@gmail.com>
2021-02-21 19:08:22 -0500

    fix audio slider

---
+++ MobileFinalProject_Platformer/Assets/Scripts/WIP/Cat/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    float lastVolume;
+
+    private void Start()
+    {
+        lastVolume = 1;
+    }
+
+    public void ToggleSound()
+    {
+        if (AudioListener.volume > 0)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = lastVolume;
+        }
+
+          //  AudioListener.volume > 0 ? AudioListener.volume = 0 : AudioListener.volume = lastVolume;
+    }
+
+    public void OnVolumeValueChanged(float newValue)
+    {
+        AudioListener.volume = newValue;
+        lastVolume = newValue;
+    }
+}
